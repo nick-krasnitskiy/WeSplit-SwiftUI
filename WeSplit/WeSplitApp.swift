de27c24ad5f7d5694aeed0e32a5d95a647af3280
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2021-11-08 11:51:55 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Nick Krasnitskiy on 08.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
